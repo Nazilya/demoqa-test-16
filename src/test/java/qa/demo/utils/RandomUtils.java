3fe685efab15bc3ce142b545ba66ee5b3475f622
Nazilya Mullagildina <mullagildina.n@yandex.ru>
2022-11-24 15:23:27 +0500

    lesson6 genTestData_byRandomGenerators

--- src/test/java/qa/demo/utils/RandomUtils.java
+++ src/test/java/qa/demo/utils/RandomUtils.java
@@ -1,28 +1,33 @@
 package qa.demo.utils;
 
+import com.github.javafaker.Faker;
 import org.apache.commons.lang3.RandomStringUtils;
-
 import java.security.SecureRandom;
 import java.util.Random;
 import java.util.UUID;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class RandomUtils {
+    Faker faker = new Faker();
+    static Random random = new Random();
+
+    /*
     public static void main(String[] args) {
-//        System.out.println(randomString(15));
-//        System.out.println(randomEmail(7));
-//        System.out.println(randomNumber(10000000L, 99999999999L));
-//        System.out.println(randomPhone("7", 10000000, 999999999));
-//
-//        System.out.println(RandomStringUtils.random(20, true, true));
-//        System.out.println(RandomStringUtils.random(20, false, false));
-//        System.out.println(RandomStringUtils.random(20, true, false));
-//        System.out.println(RandomStringUtils.random(20, false, true));
 
-//        String[] names = {"Egor", "Alex", "Oleg", "Petr"};
-//        System.out.println(randomListItem(names));
+        System.out.println(randomString(15));
+        System.out.println(randomEmail(7));
+        System.out.println(randomNumber(10000000L, 99999999999L));
+        System.out.println(randomPhone("7", 10000000, 999999999));
+
+        System.out.println(RandomStringUtils.random(20, true, true));
+        System.out.println(RandomStringUtils.random(20, false, false));
+        System.out.println(RandomStringUtils.random(20, true, false));
+        System.out.println(RandomStringUtils.random(20, false, true));
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd MMMM yyyy", Locale.ENGLISH);
+        String[] birthDay = dateFormat.format(faker.date().birthday()).split(" ");
     }
-
+*/
     public static String generateUUID() {
         String uuid = UUID.randomUUID().toString();
         return "uuid = " + uuid;
@@ -68,5 +73,24 @@ public class RandomUtils {
     public static int randInt(int min, int max) {
         Random rand = new Random();
         return rand.nextInt((max - min) + 1) + min;
+    }
+
+
+    public String getSubject() {
+        String[] subjects = {"English", "Math", "Arts", "Physics", "Chemistry", "Economics", "Biology"};
+        int index = random.nextInt(subjects.length);
+        return subjects[index];
+    }
+
+    public String getMonth() {
+        String[] arrMonth = {"January", "February", "March", "April", "May",
+                "June", "July", "August", "September", "October", "November", "December"};
+        int index = random.nextInt(arrMonth.length);
+        return arrMonth[index];
+    }
+
+    public static String getHobbies() {
+        String[] hobbies = {"Sports", "Reading", "Music"};
+        return hobbies[random.nextInt(hobbies.length)];
     }
 }
